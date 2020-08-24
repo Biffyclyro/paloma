47d95e98bb36d0a62090f30418a807b0257e805b
Biffyclyro <biffyclyro@biffyclyro.localdomain>
2020-08-24 14:38:31 -0300

    adicionando uurl do servidor

--- src/app/core/services/contact-service.service.ts
+++ src/app/core/services/contact-service.service.ts
@@ -7,7 +7,7 @@ import {Mensagem} from 'src/app/pages/contato/contato.component';
   providedIn: 'root'
 })
 export class ContactServiceService extends AbstractApiService{
-  URL_API = 'http://localhost:8090';
+  URL_API = 'https://paloma-contact.herokuapp.com';
 
   public wakeUpServer(): void {
       this.httpC.get(this.URL_API);
